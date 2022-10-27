4e3567af3e8c0328f5f5833c5120a1c4359b531c
joyshilnil <charticchandra@gmail.com>
2022-10-27 17:05:04 +0600

    readme file added

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -4,6 +4,13 @@ const Courses = () => {
     return (
         <>
             <div className="main-container">
+
+
+                
+
+
+
+
                 <div className="left-side shadow p-4 rounded me-3">
                     <h3>All Courses 06</h3>
                     <p className='p-3 shadow text-primary bolder mt-3 rounded cursor-pointer'>Courses One</p>
@@ -13,9 +20,15 @@ const Courses = () => {
                     <p className='p-3 shadow text-primary bolder mt-3 rounded'>Courses One</p>
                     <p className='p-3 shadow text-primary bolder mt-3 rounded'>Courses One</p>
                 </div>
+
+
+
                 <div className="right-side">
                     <h1>This is right side</h1>
                 </div>
+
+
+
             </div>
         </>
     );
